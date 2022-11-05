6946ccce3fddf0f2b51e2013f98022e09f1a8e7b
Miro Rihu <miro.rihu@outlook.com>
2022-11-05 23:47:29 +0200

    Git pull ja toimintaan saanti, npm refactorointia.

--- aikataulujen-sovittaja/src/App.js
+++ aikataulujen-sovittaja/src/App.js
@@ -1,27 +1,27 @@
-import getCalendar from './services/getCalendar'
-import {useState} from 'react'
-import FetchCalendarForm from './components/FetchCalendarForm'
+import getCalendar from "./services/getCalendar";
+import { useState } from "react";
+import FetchCalendarForm from "./components/FetchCalendarForm";
 
 const App = () => {
-  const [kasiteltavaKalenteri, setKasiteltavaKalenteri] = useState('') //tänne tallennetaan käsiteltävä kalenteri tekstinä
-  const [kalenteriUrl, setUrl] = useState('') //url laatikkoa varten
+  const [kasiteltavaKalenteri, setKasiteltavaKalenteri] = useState(""); //tänne tallennetaan käsiteltävä kalenteri tekstinä
+  const [kalenteriUrl, setUrl] = useState(""); //url laatikkoa varten
 
   const handleDownload = (event) => {
-    event.preventDefault()
-    getCalendar
-      .download(kalenteriUrl, setKasiteltavaKalenteri)
-      console.log("kalenteri: " + kasiteltavaKalenteri)
-  }
-    
+    event.preventDefault();
+    getCalendar.download(kalenteriUrl, setKasiteltavaKalenteri);
+    console.log("kalenteri: " + kasiteltavaKalenteri);
+  };
+
   //demon vuoksi laitetaan kasiteltavaKalenteri näkyviin sivulle
   return (
     <div>
       <div>
-    <FetchCalendarForm 
-    kalenteriUrl={kalenteriUrl} 
-    handleKalenteriUrlChange={({ target }) => setUrl(target.value)} 
-    handleFetchCalendar={handleDownload}
-    />
+        <FetchCalendarForm
+          kalenteriUrl={kalenteriUrl}
+          handleKalenteriUrlChange={({ target }) => setUrl(target.value)}
+          handleFetchCalendar={handleDownload}
+        />
+      </div>
     </div>
   );
 };
