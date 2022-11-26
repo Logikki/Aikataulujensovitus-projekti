2065a69cccdb8206da71be1e40e37305c25c34e6
Roni Koskinen <roni.koskinen@gmail.com>
2022-11-26 14:01:45 +0200

    calendarview lisätty

---
+++ aikataulujen-sovittaja/src/components/CalendarView.js
@@ -0,0 +1,11 @@
+//Tänne tulee kaikki, mitä näytetään kun on kirjauduttu sisään kalenteriin
+const CalendarView = () => {
+
+    return (
+        <div>
+            calendarView
+        </div>
+    )
+}
+
+export default CalendarView
