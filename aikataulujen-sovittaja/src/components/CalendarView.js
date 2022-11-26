6a40c7770e9584597448d501d05af40c5c8d0d1b
pejamant <pekka.j.mantysaari@student.jyu.fi>
2022-11-26 15:04:23 +0200

    DayPilotin lisäys

--- aikataulujen-sovittaja/src/components/CalendarView.js
+++ aikataulujen-sovittaja/src/components/CalendarView.js
@@ -1,15 +1,16 @@
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
 //Tänne tulee kaikki, mitä näytetään kun on kirjauduttu sisään kalenteriin
 const CalendarView = (sharedCalendar) => {
-    if (sharedCalendar == null) {
-        return 
-    }
-    else {
-        return (
-            <div>
-                calendarView
-            </div>
-        )
-    }
-}
+  if (sharedCalendar == null) {
+    return;
+  } else {
+    return (
+      <div>
+        calendarView
+        <DayPilotCalendar viewType="Week" />
+      </div>
+    );
+  }
+};
 
-export default CalendarView
+export default CalendarView;
