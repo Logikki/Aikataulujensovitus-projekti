5e659ae0e00369711c658c87e998435ebf3c6d35
Miro Rihu <miro.rihu@outlook.com>
2022-12-05 22:57:05 +0200

    .env tiedoston lisäys takaisin

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const app = require('./app')
-const http = require('http')
-const logger = require('./utils/logger')
-require('dotenv').config()
-const PORT = process.env.PORT
-const server = http.createServer(app)
+const app = require("./app");
+const http = require("http");
+const logger = require("./utils/logger");
+require("dotenv").config();
+const PORT = process.env.PORT;
+const server = http.createServer(app);
 server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
