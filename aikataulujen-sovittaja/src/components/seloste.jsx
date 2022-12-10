dde8424ebee180ae3ebd75ee1a0b8531f874c551
pejamant <pekka.j.mantysaari@student.jyu.fi>
2022-12-10 15:03:05 +0200

    Tietosuojaseloste

--- aikataulujen-sovittaja/src/components/seloste.jsx
+++ aikataulujen-sovittaja/src/components/seloste.jsx
@@ -4,21 +4,6 @@ const seloste = (props) => {
   const buttonWidth = 120;
   const right = 15;
 
-  const tietoseloste =
-   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
   return (
     <div className="box" clickable="false">
       <div className="seloste-box">
@@ -50,7 +35,56 @@ const seloste = (props) => {
         </button>
         <>
           <h1>Tietosuojaseloste</h1>
-          <div>{tietoseloste}</div>
+          <div>
+            <p>
+              <strong>Palvelun käyttämisen ehdot</strong>
+            </p>
+            <p>
+              Hyväksymällä tietosuojailmoituksen ja antamalla palvelun käyttöön
+              kalenteritiedot hyväksyt mahdollisuuden, että kalenteritietosi ovat vapaiden
+              aikojesi osalta nähtävissä muille saman yhdistetyn kalenterin käyttäjille.
+            </p>
+            <p>
+              <strong>Palvelussa käsiteltävät henkilötiedot</strong>
+            </p>
+            <p>
+              Palvelun käyttämisessä sinulta pyydetään seuraavia tietoja: henkilökohtaisen
+              kalenterin URL-osoite ja henkilökohtainen kalenteridata. Palvelun
+              käyttäminen ei vaadi käyttäjältä yksilöiviä henkilötietoja kalenteritietojen
+              ja url:n lisäksi. Kalenteritiedoista poistetaan kaikki suorat tunnistetiedot
+              sekä käyttäjästä että kalenteritapahtumien sisällöistä.
+            </p>
+            <p>
+              <strong>Miksi käyttäjätietoja kerätään?</strong>
+            </p>
+            <p>
+              Kalenterisovelluksen käyttämiseen tarvitaan henkilökohtaisten
+              kalenteritietojen lataamista sovellukseen. Käyttäjän lataamien
+              kalenteritietojen avulla palvelussa voidaan tarjota näkymä eri käyttäjien
+              yhteisistä vapaista ajoista.
+            </p>
+            <p>
+              <strong>Kenellä on pääsy tietoihin?</strong>
+            </p>
+            <p>
+              Palvelussa on mahdollista muodostaa useiden käyttäjien yhdistetty kalenteri.
+              Yhdistetyn kalenterin käyttäjät näkevät samaa yhdistettyä kalenteria
+              käyttävien käyttäjien keskinäiset yhteiset vapaat ajat. Jokainen yhdistetty
+              kalenteri on salasanasuojattu.
+            </p>
+            <p>
+              <strong>Tietojen säilyttäminen</strong>
+            </p>
+            <p>
+              Käyttäjän lataamat kalenteritiedot säilytetään kuusi kuukautta palveluun
+              lataamishetkestä eteenpäin, jonka jälkeen tiedot poistetaan automaattisesti.
+              Käyttäjällä on aina mahdollisuus poistaa omat kalenteritietonsa palvelusta.
+            </p>
+            <p>
+              <strong>Tietojen luovutukset</strong>
+            </p>
+            <p>Palveluun syötettyjä tietoja ei luovuteta kaupallisiin tarkoituksiin.</p>
+          </div>
         </>
       </div>
     </div>
