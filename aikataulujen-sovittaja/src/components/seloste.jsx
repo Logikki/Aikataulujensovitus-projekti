5cb69068f17a3661a8baabd39e90bdf1f11c7eec
pejamant <pekka.j.mantysaari@student.jyu.fi>
2022-12-10 13:47:21 +0200

    Selosteteksti siirretty selostecomppiin

--- aikataulujen-sovittaja/src/components/seloste.jsx
+++ aikataulujen-sovittaja/src/components/seloste.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const seloste = (props) => {
+  const tietoseloste =
+   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
   return (
     <div className="box" clickable="false">
       <div className="seloste-box">
@@ -30,7 +45,10 @@ const seloste = (props) => {
         >
           {props.left}
         </button>
-        {props.content}
+        <>
+          <h1>Tietosuojaseloste</h1>
+          <div>{tietoseloste}</div>
+        </>
       </div>
     </div>
   );
